4e538b98c9443624326a0725eb7e677640f2618b
Eunsung_Choi <ens454@naver.com>
2021-01-24 23:32:56 +0900

    그래프

---
+++ baekjoon/2738.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int N, M;
+    scanf("%d %d",&N,&M);
+    int A[N][M], B[N][M], C[N][M];
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            scanf("%d",&A[i][j]);
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            scanf("%d",&B[i][j]);
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
